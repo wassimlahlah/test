f1820cf330fe58c0439a58f219f397d432e2581d
wassimlahlah <wassimlahlah06@gmail.com>
2025-09-12 18:18:25 +0100

    __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ def create_app():
     app = Flask(__name__)
   
     app.config["SECRET_KEY"] =os.getenv("SECRET_KEY")
-    app.config["SQLALCHEMY_DATABASE_URI"] =os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] =os.getenv("DATABASE_URL","sqlite:///app.db")
     # تهيئة الإضافات
     db.init_app(app)
     migrate.init_app(app, db)
